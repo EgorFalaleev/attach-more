213b405935aa0a14ec9312edbd2fbb337253c832
EgorF <egor1268@gmail.com>
2025-01-22 06:09:59 +0500

    add project context with BootstrapInstaller

---
+++ Assets/_Project/Code/Runtime/Infrastructure/Installers/BootstrapInstaller.cs
@@ -0,0 +1,16 @@
+using Zenject;
+
+namespace Runtime.Infrastructure
+{
+    public class BootstrapInstaller : MonoInstaller, IInitializable
+    {
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesTo<BootstrapInstaller>().FromInstance(this).AsSingle();
+        }
+
+        public void Initialize()
+        {
+        }
+    }
+}
