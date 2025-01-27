2da8b27af9240162385fe33fdea4af9ba78f4149
EgorF <egor1268@gmail.com>
2025-01-27 06:30:39 +0500

    implement player movement

---
+++ Assets/_Project/Code/Runtime/Gameplay/Player/PlayerView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Runtime.Services;
+using UnityEngine;
+using Zenject;
+
+namespace Runtime.Gameplay.Player
+{
+    public class PlayerView : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 5f;
+        
+        private IInputService _inputService;
+
+        [Inject]
+        private void Construct(IInputService inputService)
+        {
+            _inputService = inputService;
+        }
+
+        private void Update()
+        {
+            if (_inputService.MoveDirection != Vector3.zero)
+                Move(_inputService.MoveDirection * (_speed * Time.deltaTime));
+        }
+
+        private void Move(Vector3 direction)
+        {
+            transform.Translate(direction);
+        }
+    }
+}
