4f4300bf06e58b88c806497365fa10fc876bbdd4
EgorF <egor1268@gmail.com>
2025-01-30 06:29:24 +0500

    prototype weapon spawner

---
+++ Assets/_Project/Code/Runtime/Gameplay/Weapon/Spawner/WeaponSpawner.cs
@@ -0,0 +1,49 @@
+﻿using Runtime.Gameplay.Player;
+using Runtime.Gameplay.Weapon.Factory;
+using UnityEngine;
+using Zenject;
+
+namespace Runtime.Gameplay.Weapon.Spawner
+{
+    public class WeaponSpawner : ITickable
+    {
+        private readonly IWeaponFactory _weaponFactory;
+        private readonly Transform _playerTransform;
+
+        private float _spawnCooldown = 1f;
+        private float _timeFromLastSpawn;
+        private float _spawnRadius = 5f;
+        
+        public WeaponSpawner(IWeaponFactory weaponFactory, PlayerView playerView)
+        {
+            _weaponFactory = weaponFactory;
+            _playerTransform = playerView.transform;
+            Debug.Log("ctor");
+        }
+        
+        public void Tick()
+        {
+            Debug.Log("Tick");
+            _timeFromLastSpawn += Time.deltaTime;
+
+            if (_timeFromLastSpawn >= _spawnCooldown)
+            {
+                _timeFromLastSpawn = 0f;
+                var randomPosition = GetRandomPositionAroundPlayer();
+                _weaponFactory.CreateWeapon(randomPosition);
+            }
+        }
+        
+        private Vector3 GetRandomPositionAroundPlayer()
+        {
+            var randomOffset = Random.insideUnitCircle * _spawnRadius;
+            var playerPosition = _playerTransform.position;
+
+            return new Vector3(
+                playerPosition.x + randomOffset.x,
+                1f,
+                playerPosition.z + randomOffset.y
+            );
+        }
+    }
+}
--- Assets/_Project/Code/Runtime/Infrastructure/Installers/LevelInstaller.cs
+++ Assets/_Project/Code/Runtime/Infrastructure/Installers/LevelInstaller.cs
@@ -1,13 +1,20 @@
 ﻿using Runtime.Gameplay.Attachment;
+using Runtime.Gameplay.Player;
+using Runtime.Gameplay.Weapon.Spawner;
+using UnityEngine;
 using Zenject;
 
 namespace Runtime.Infrastructure.Installers
 {
     public class LevelInstaller : MonoInstaller
     {
+        [SerializeField] private PlayerView _playerView;
+        
         public override void InstallBindings()
         {
             Container.BindInterfacesAndSelfTo<AttachmentController>().AsSingle().NonLazy();
+            Container.Bind<PlayerView>().FromInstance(_playerView).AsSingle();
+            Container.BindInterfacesAndSelfTo<WeaponSpawner>().AsSingle().NonLazy();
         }
     }
 }
