60f5326a4fa3ba13a4c92b78d60cb59014ef62ee
EgorF <egor1268@gmail.com>
2025-01-31 06:23:29 +0500

    add AttachZone

---
+++ Assets/_Project/Code/Runtime/Gameplay/Attachment/AttachZone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Runtime.Gameplay.Attachment
+{
+    [RequireComponent(typeof(Collider))]
+    public class AttachZone : MonoBehaviour
+    {
+        public event Action<AttachZone> OnAttachableInRange;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out AttachZone attachZone))
+            {
+                OnAttachableInRange?.Invoke(attachZone);
+            }
+        }
+    }
+}
