54c919a7c1c7c3bba11d0dd12e2323082c66e4ec
EgorF <egor1268@gmail.com>
2025-02-14 06:28:57 +0500

    add player factory

---
+++ Assets/_Project/Code/Runtime/Gameplay/Player/Factory/IPlayerFactory.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Runtime.Gameplay.Player.Factory
+{
+    public interface IPlayerFactory
+    {
+        void CreatePlayer(Vector3 position);
+    }
+}
---
+++ Assets/_Project/Code/Runtime/Gameplay/Player/Factory/PlayerFactory.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Zenject;
+using Quaternion = UnityEngine.Quaternion;
+
+namespace Runtime.Gameplay.Player.Factory
+{
+    public class PlayerFactory : IPlayerFactory
+    {
+        private readonly PlayerView _playerView;
+        private readonly IInstantiator _instantiator;
+
+        public PlayerFactory(PlayerView playerView, IInstantiator instantiator)
+        {
+            _playerView = playerView;
+            _instantiator = instantiator;
+        }
+
+        public void CreatePlayer(Vector3 position)
+        {
+            _instantiator.InstantiatePrefabForComponent<PlayerView>(_playerView, position, Quaternion.identity, null);
+        }
+    }
+}
