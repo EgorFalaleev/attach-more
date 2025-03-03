27bc6f3b069a90b94a3b6aae644dfe2c1beecd24
EgorF <egor1268@gmail.com>
2025-03-03 07:13:04 +0500

    create enemy view from path

--- Assets/_Project/Code/Runtime/Gameplay/Enemies/Factory/EnemyViewFactory.cs
+++ Assets/_Project/Code/Runtime/Gameplay/Enemies/Factory/EnemyViewFactory.cs
@@ -1,21 +1,25 @@
-﻿using Zenject;
+﻿using Runtime.Constants;
+using Runtime.Infrastructure.Assets;
+using Zenject;
 
 namespace Runtime.Gameplay.Enemies.Factory
 {
     public class EnemyViewFactory : IEnemyViewFactory
     {
-        private readonly EnemyView _enemyPrefab;
+        private readonly IAssetProvider _assetProvider;
         private readonly IInstantiator _instantiator;
 
-        public EnemyViewFactory(EnemyView enemyPrefab, IInstantiator instantiator)
+        public EnemyViewFactory(IAssetProvider assetProvider, IInstantiator instantiator)
         {
-            _enemyPrefab = enemyPrefab;
+            _assetProvider = assetProvider;
             _instantiator = instantiator;
         }
 
         public EnemyView CreateView(Enemy enemy)
         {
-            var view = _instantiator.InstantiatePrefabForComponent<EnemyView>(_enemyPrefab);
+            var prefab = _assetProvider.Load<EnemyView>(Paths.Enemy);
+            
+            var view = _instantiator.InstantiatePrefabForComponent<EnemyView>(prefab);
             view.Initialize(enemy);
             return view;
         }
