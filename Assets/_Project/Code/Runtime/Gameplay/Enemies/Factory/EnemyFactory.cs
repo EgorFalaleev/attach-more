f41f235cdd3ea6b910938363b95d4498d25a2c1e
EgorF <egor1268@gmail.com>
2025-03-04 06:18:21 +0500

    add enemy to provider after spawn

--- Assets/_Project/Code/Runtime/Gameplay/Enemies/Factory/EnemyFactory.cs
+++ Assets/_Project/Code/Runtime/Gameplay/Enemies/Factory/EnemyFactory.cs
@@ -1,4 +1,5 @@
-﻿using Runtime.Gameplay.Player.Provider;
+﻿using Runtime.Gameplay.Enemies.Provider;
+using Runtime.Gameplay.Player.Provider;
 using UnityEngine;
 
 namespace Runtime.Gameplay.Enemies.Factory
@@ -6,19 +7,22 @@ namespace Runtime.Gameplay.Enemies.Factory
     public class EnemyFactory : IEnemyFactory
     {
         private readonly IPlayerViewProvider _playerViewProvider;
+        private readonly IEnemiesProvider _enemiesProvider;
 
-        public EnemyFactory(IPlayerViewProvider playerViewProvider)
+        public EnemyFactory(IPlayerViewProvider playerViewProvider, IEnemiesProvider enemiesProvider)
         {
             _playerViewProvider = playerViewProvider;
+            _enemiesProvider = enemiesProvider;
         }
 
         public Enemy CreateEnemy(Vector3 position)
         {
-            return new Enemy
-            {
-                Position = position,
-                TargetPosition = _playerViewProvider.PlayerView.transform.position
-            };
+            var enemy = new Enemy()
+                { Position = position, TargetPosition = _playerViewProvider.PlayerView.transform.position };
+
+            _enemiesProvider.AddEnemy(enemy);
+            
+            return enemy;
         }
     }
 }
