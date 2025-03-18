baebe885f1b567e2a6b351212381eef583887eda
EgorF <egor1268@gmail.com>
2025-03-18 06:21:02 +0500

    create interface for attachable logic, add Weapon class for weapon logic

---
+++ Assets/_Project/Code/Runtime/Gameplay/Attachment/IAttachable.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Runtime.Gameplay.Attachment
+{
+    public interface IAttachable
+    {
+        bool IsAttached { get; }
+        float AttachmentRadius { get; }
+        void Attach(Transform parent, Vector3 offset);
+    }
+}
---
+++ Assets/_Project/Code/Runtime/Gameplay/Weapon/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using Runtime.Gameplay.Attachment;
+using UnityEngine;
+
+namespace Runtime.Gameplay.Weapon
+{
+    public class Weapon : IAttachable
+    {
+        public bool IsAttached => _isAttached;
+        public float AttachmentRadius { get; }
+
+        private Transform _parent;
+        private Vector3 _offset;
+        private bool _isAttached;
+
+        public Weapon(float attachmentRadius)
+        {
+            AttachmentRadius = attachmentRadius;
+        }
+        
+        public void Attach(Transform parent, Vector3 offset)
+        {
+            _parent = parent;
+            _offset = offset;
+            _isAttached = true;
+        }
+
+        public Vector3 GetAttachedPosition =>
+            _parent.position + _offset;
+    }
+}
